a48be1ef8afd9f9287e593422b8871d002d0e44f
matloobhasnain <matloobhasnain@yahoo.com>
2014-09-01 14:56:49 +0500

    Subcategory collection view implemented

--- Naat-E-Rasool/SubCategoriesVC.m
+++ Naat-E-Rasool/SubCategoriesVC.m
@@ -9,6 +9,7 @@
 #import "SubCategoriesVC.h"
 #import "CategoryCell.h"
 #import "ParameterDTO.h"
+#import "CategoryCollectionViewCell.h"
 @interface SubCategoriesVC ()
 
 @end
@@ -35,26 +36,56 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+//{
+//    return self.subCategoryArray.count;
+//}
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    NSString * cellId = @"categoryCell";
+//    CategoryCell* catCell = (CategoryCell*)[tableView dequeueReusableCellWithIdentifier:cellId];
+//    if(catCell == nil)
+//    {
+//        NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"CategoryCell" owner:self options:nil];
+//        catCell = [nib objectAtIndex:0];
+//    }
+//    CategoryDTO *cat = [self.subCategoryArray objectAtIndex:indexPath.row];
+//    
+//    catCell.lblCategoryName.text = cat.categoryName;
+//    
+//    return catCell;
+//}
+//- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    CategoryDTO *cat = [self.subCategoryArray objectAtIndex:indexPath.row];
+//    ParameterDTO *param = [[ParameterDTO alloc]init];
+//    param.sub_CategoryID = cat.categoryID;
+//    param.content_Type = @"video";
+//    param.offSet = 1;
+//    param.language = @"English";
+//    [[NetworkLayer sharedNetworkLayer]getAllVideoesForSubCategory:param];
+//}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
     return self.subCategoryArray.count;
 }
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSString * cellId = @"categoryCell";
-    CategoryCell* catCell = (CategoryCell*)[tableView dequeueReusableCellWithIdentifier:cellId];
-    if(catCell == nil)
-    {
-        NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"CategoryCell" owner:self options:nil];
-        catCell = [nib objectAtIndex:0];
-    }
+    NSString * cellId = @"category";
+    CategoryCollectionViewCell* catCell = (CategoryCollectionViewCell*)[collectionView dequeueReusableCellWithReuseIdentifier:cellId forIndexPath:indexPath];
+    
     CategoryDTO *cat = [self.subCategoryArray objectAtIndex:indexPath.row];
     
-    catCell.lblCategoryName.text = cat.categoryName;
+    catCell.lbl_Name.text = cat.categoryName;
     
     return catCell;
+    
+    
 }
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
     CategoryDTO *cat = [self.subCategoryArray objectAtIndex:indexPath.row];
     ParameterDTO *param = [[ParameterDTO alloc]init];
